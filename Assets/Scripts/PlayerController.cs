cf5334e5f1aa789a74a880376992f63ca572f8f0
Md Ikram Khan Johan <68727970+Ikram-Khan-Johan@users.noreply.github.com>
2025-06-09 13:38:11 +0600

    feat: smooth camera controller setup with cinemachine, following player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private CharacterController characterController;
     [SerializeField] private float speed = 5f;
-
+    [SerializeField] private float rotationSpeed = 0.1f; // Degrees per second
     // Update is called once per frame
     void Update()
     {
@@ -13,12 +13,20 @@ public class PlayerController : MonoBehaviour
 
     void HandleMovement()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        Vector3 move = transform.right * x + transform.forward * z;
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        Vector3 move = new Vector3(x, 0, z).normalized;
         if (move.magnitude >= 0.1f)
         {
-           characterController.Move(move * speed * Time.deltaTime);
+            // Calculate the target angle based on the input direction
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
+
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSpeed, 0.1f);
+            // Rotate the player to face the direction of movement
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+            // Move the character controller in the specified direction
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+           characterController.Move(moveDirection * speed * Time.deltaTime);
         }
     }
 }
