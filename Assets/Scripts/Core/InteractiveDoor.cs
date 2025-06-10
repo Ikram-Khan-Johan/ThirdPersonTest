24d8adf18d42bb0957c859446ce623737dd93275
Md Ikram Khan Johan <68727970+Ikram-Khan-Johan@users.noreply.github.com>
2025-06-11 03:43:08 +0600

    feat: added door sound

--- Assets/Scripts/Core/InteractiveDoor.cs
+++ Assets/Scripts/Core/InteractiveDoor.cs
@@ -6,6 +6,7 @@ public class InteractiveDoor : MonoBehaviour, IInteractable
     // It allows interaction with the door, such as opening or closing it.
 
     [SerializeField] private Animator animator;
+    ISoundPlayer soundPlayer;
     bool isOpen = false;
     public void Interact()
     {
@@ -13,6 +14,11 @@ public class InteractiveDoor : MonoBehaviour, IInteractable
         Debug.Log("Interacted with door by: " );
         // For example, toggle the door's open/close state
         ToggleDoor();
+    }
+
+    void Start()
+    {
+        soundPlayer = GetComponent<ISoundPlayer>();
     }
 
     // void Update()
@@ -34,6 +40,8 @@ public class InteractiveDoor : MonoBehaviour, IInteractable
             animator.SetBool("isOpen", false);
             isOpen = false;
             GetComponent<BoxCollider>().isTrigger = false;
+            AudioClip audioClip = Resources.Load<AudioClip>("Door_Close");
+            soundPlayer?.PlaySound(audioClip);
             Debug.Log("Door closed.");
         }
         else
@@ -41,6 +49,8 @@ public class InteractiveDoor : MonoBehaviour, IInteractable
             animator.SetBool("isOpen", true);
             isOpen = true;
             GetComponent<BoxCollider>().isTrigger = true;
+             AudioClip audioClip = Resources.Load<AudioClip>("Door_Open");
+            soundPlayer?.PlaySound(audioClip);
             Debug.Log("Door opened.");
         }
         // Logic to toggle the door's state (open/close)
