1f8c21ccc1ca1ffd45520b3fc5084d092c5b183e
Md Ikram Khan Johan <68727970+Ikram-Khan-Johan@users.noreply.github.com>
2025-06-11 15:47:16 +0600

    feat: added instruction UI

--- Assets/Scripts/UI/GameUIManager.cs
+++ Assets/Scripts/UI/GameUIManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -5,17 +6,29 @@ public class GameUIManager : MonoBehaviour, IMessage
 {
     [SerializeField] private GameObject messagePanel;
     [SerializeField] private TMP_Text messageText;
+    [SerializeField] private float hintMessageDelay = 3f; // Delay before hiding the hint message
+    [SerializeField] private GameObject hintPanel;
 
+    void Start()
+    {
+        StartCoroutine(HintMessage(hintMessageDelay));
+    }
     public void HideMessage()
     {
         messagePanel.SetActive(false);
         messageText.text = string.Empty;
-       // Debug.Log("Message hidden.");
+        // Debug.Log("Message hidden.");
     }
     public void ShowMessage(string message)
     {
         messagePanel.SetActive(true);
         messageText.text = message;
-       // Debug.Log("Message displayed: " + message);
+        // Debug.Log("Message displayed: " + message);
+    }
+    
+    IEnumerator HintMessage(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        hintPanel.SetActive(false);
     }
 }
